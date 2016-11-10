008f6f5e6933d6668bda4cc5cc670d217c70ac1e
Showryapagidimarry <showryapagidimarry@gmail.com>
2016-11-11 02:54:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,47 +12,47 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/css/about.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'about.css'));
+app.get('/css/about.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'about.css'));
 });
-app.get('/ui/css/grid.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'grid.css'));
+app.get('/css/grid.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'grid.css'));
 });
-app.get('/ui/css/ie.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ie.css'));
+app.get('/css/ie.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'ie.css'));
 });
-app.get('/ui/css/main.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+app.get('/css/main.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'main.css'));
 });
-app.get('/ui/css/modal.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'modal.css'));
+app.get('/css/modal.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'modal.css'));
 });
-app.get('/ui/css/normalize.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'normalize.css'));
+app.get('/css/normalize.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'normalize.css'));
 });
-app.get('/ui/css/owl.corousel.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'owl.corousel.css'));
+app.get('/css/owl.corousel.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'owl.corousel.css'));
 });
-app.get('/ui/js/bootstrap.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.js'));
+app.get('/js/bootstrap.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'bootstrap.min.js'));
 });
-app.get('/ui/js/dropdown.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'dropdown.js'));
+app.get('/js/dropdown.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'dropdown.js'));
 });
-app.get('/ui/js/jquery.validate.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'jquery.validate.js'));
+app.get('/js/jquery.validate.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'jquery.validate.js'));
 });
-app.get('/ui/js/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/js/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'js', 'main.js'));
 });
-app.get('/ui/js/vendor/modernizr.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'modernizr.js'));
+app.get('/js/vendor/modernizr.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'vendor', 'modernizr.js'));
 });
-app.get('/ui/js/vendor/owl.carousel.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'owl.carousel.min.js'));
+app.get('/js/vendor/owl.carousel.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'vendor', 'owl.carousel.min.js'));
 });
-app.get('/ui/js/vendor/respond.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'respond.min.js'));
+app.get('/js/vendor/respond.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'vendor', 'respond.min.js'));
 });
 
 
